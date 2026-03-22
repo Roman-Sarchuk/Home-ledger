12fcde8ed5d5933b2b33d7a45c236d3e27ff9ff6
Roman-Sarchuk <91730539+Roman-Sarchuk@users.noreply.github.com>
2026-03-22 16:01:04 +0200

    refactor: remove commented route connections from app and server files

--- backend/app.js
+++ backend/app.js
@@ -7,4 +7,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect routes
+// app.use('/api/auth', require('./routes/authRoutes'));
+// app.use('/api/accounts', require('./routes/accountRoutes'));
+// app.use('/api/categories', require('./routes/categoryRoutes'));
+// app.use('/api/transactions', require('./routes/transactionRoutes'));
+
+// root route
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'API is working' });
+});
+
+
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,6 @@ const PORT = process.env.PORT || 3000;
 // connect to DB
 connectDB();
 
-// Connect routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/accounts', require('./routes/accountRoutes'));
-app.use('/api/categories', require('./routes/categoryRoutes'));
-app.use('/api/transactions', require('./routes/transactionRoutes'));
-
 app.listen(PORT, () => {
     console.log(`[LOG]: Server is running on port ${PORT}`);
 });
